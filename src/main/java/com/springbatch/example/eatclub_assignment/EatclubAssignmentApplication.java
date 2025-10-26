26b3ecbe70a64be7504feb9d8146e4a0f985b47f
mu-droid <mudit12131@gmail.com>
2025-10-26 19:15:39 +0530

    add initial Spring Boot project structure with empty packages

---
+++ src/main/java/com/springbatch/example/eatclub_assignment/EatclubAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.springbatch.example.eatclub_assignment;
+
+import lombok.Getter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatclubAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatclubAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbatch/example/eatclub_assignment/EatclubAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.example.eatclub_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatclubAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
